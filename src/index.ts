07e5edbed7fd9a0b7aecf6389131b056af9f9f60
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-12-11 15:53:19 +0700

    Update default log level

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { program } from 'commander'
 import 'dotenv/config'
 import { discord } from './clients/discord'
+import { logger } from './logger'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require('../package.json')
@@ -8,7 +9,16 @@ const pkg = require('../package.json')
 program
   .version(pkg.version)
 
-program.action(() => {
+program.action((args) => {
+  if (args.debug) {
+    // eslint-disable-next-line dot-notation
+    const transports = logger.transports.filter((v) => v['name'] === 'console')
+    transports.forEach((transport) => {
+      // eslint-disable-next-line no-param-reassign
+      transport.level = 'silly'
+    })
+  }
+
   discord.start()
 })
 
