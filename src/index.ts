9ccd951048bf3478796933762341161e2dea4ab8
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-12-25 22:55:12 +0700

    Add download command

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import 'dotenv/config'
 import { discord } from './clients/discord'
 import { logger } from './logger'
 import { configManager } from './modules/ConfigManager'
+import { YouTubeChat } from './modules/YouTubeChat'
+import { YouTubeUtil } from './utils/YouTubeUtil'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require('../package.json')
@@ -11,6 +13,10 @@ program
   .version(pkg.version)
   .option('-d, --debug', 'Show debug logs')
   .option('-c, --cookies <FILE_PATH>', 'File to load cookies, default to ./cookies.txt')
+  .command('download <URL>')
+  .action((url) => {
+    new YouTubeChat(YouTubeUtil.getVideoId(url)).start()
+  })
 
 program.action(async (args) => {
   if (args.debug) {
